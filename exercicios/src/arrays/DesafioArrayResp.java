4c89a68e878df44ddab39e4dc7905f00b9426a4e
Emerson <emersonsdossantos@gmail.com>
2023-06-26 19:57:22 -0300

    Arrays e Collections: Desafio Resposta

---
+++ exercicios/src/arrays/DesafioArrayResp.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class DesafioArrayResp {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Informe a quantidade de notas: ");
+		int qtdeNotas = entrada.nextInt();
+		
+		double[] notas = new double [qtdeNotas];
+
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println("Informe a nota " + (i + 1) + ":");
+			notas[i] = entrada.nextDouble();
+		}	
+		
+		double total = 0;
+		for (double nota : notas) {
+			total += nota;
+		}
+		
+		double media = total / notas.length;
+		System.out.println("Media de nota = " + media);
+		entrada.close();
+	}
+
+}
